4c31b780f1e971814faa026dbb28ff0eb86938d6
SeungYn <kwls0407@gmail.com>
2024-06-03 00:05:53 +0900

    remove: 로그 제거

--- main.js
+++ main.js
@@ -70,10 +70,7 @@ const sceneInfo = [
 function fadeInFakerImage() {
   const objs = sceneInfo[0].objs;
   const { width, height } = objs.canvas.getBoundingClientRect();
-  const visitedMap = Array.from({ length: height }, () =>
-    new Array(width).fill(false)
-  );
-  console.log(visitedMap);
+
   const tempCanvas = document.createElement('canvas');
   const ctx = tempCanvas.getContext('2d');
 
@@ -86,94 +83,41 @@ function fadeInFakerImage() {
   objs.canvas.width = width;
   objs.canvas.height = height;
 
-  //objs.context.putImageData(imageData, 0, 0, 3000, 0, 100, 100);
-  // objs.context.putImageData(imageData, 0, 0, 100, 100, 100, 100);
-  // objs.context.putImageData(imageData, 0, 0, 200, 100, 100, 100);
-  // objs.context.putImageData(imageData, 0, 0, 300, 100, 100, 100);
-  // objs.context.putImageData(imageData, 0, 0, 400, 100, 100, 100);
-  // objs.context.putImageData(imageData, 0, 0, 0, 200, 100, 100);
-  // objs.context.putImageData(imageData, 0, 0, 0, 300, 100, 100);
-  //objs.context.drawImage(objs.introImages[0], 0, 0, width, height);
-  //console.log(objs.context.getImageData(0, 0, width, height));
   let x = 0,
     y = 0;
   let px = 0,
     py = 0;
 
-  // for (let y = 0; y < objs.canvas.height; y++) {
-  //   for (let x = 0; x < objs.canvas.width; x++) {
-  //     // (현재 현재 행 * 한 행의 바이트 수 + 구하고자하는 위치) * 4
-  //     // const pixelIndex = (y * objs.canvas.width + x) * 4;
-  //     // const red = imageData.data[pixelIndex];
-  //     // const green = imageData.data[pixelIndex + 1];
-  //     // const blue = imageData.data[pixelIndex + 2];
-  //     // const alpha = imageData.data[pixelIndex + 3];
-
-  //     // // 픽셀 그리기
-  //     // objs.context.fillStyle = `rgba(${red}, ${green}, ${blue}, ${
-  //     //   alpha / 255
-  //     // })`;
-
-  //     // objs.context.fillRect(x, y, 1, 1);
-  //     if (!visitedMap[x][y]) {
-  //       requestAnimationFrame(() => {
-  //         bfs(x, y, 1);
-  //       });
-  //     }
-  //   }
-  // }
-  //fadeInRequestAnimaion();
-  //bfs(0, 0, 40);
-
-  //fillPixel(x, y);
-  //fadeInRequestAnimaionPutImage();
   const fillRange = 20;
-  const setArr = [];
+
   fadeInRequestAnimaionPutImageWave();
 
   function fadeInRequestAnimaionPutImageWave() {
-    console.log(x, y, objs.canvas.width, objs.canvas.height);
     //if (y >= objs.canvas.height || x >= objs.canvas.width) return;
 
     objs.context.putImageData(imageData, 0, 0, x, y, fillRange, fillRange);
-    // 비동기 로직돌릴 때 x,y, 가 해당 시점의 값으로 시작이 안됨 이것은 클로저 활용
-    // setTimeout(() => {
-    //   prevPutImage(x, y, fillRange);
-    // }, 0.5);
-    prevPutImage(x, y, fillRange);
-    if (x < objs.canvas.width) x += fillRange;
-    else y += fillRange;
 
-    if (y > objs.canvas.height && x > objs.canvas.width) {
-      setArr.forEach((id) => clearInterval(id));
-    }
+    prevPutImage(x, y, fillRange);
+
+    if (x < objs.canvas.width) x += fillRange;
+    else y += fillRange; // 나머지 빈 곳 처리
 
     if (y < objs.canvas.height && x < objs.canvas.width) {
       window.requestAnimationFrame(() => {
         fadeInRequestAnimaionPutImageWave();
       });
-
-      // setInterval 상용
-      // const timeOutId = window.setInterval(() => {
-      //   fadeInRequestAnimaionPutImageWave();
-      // }, 0.5);
-      // setArr.push(timeOutId);
     } else if (y < objs.canvas.height && x > objs.canvas.width) {
       window.requestAnimationFrame(() => {
         fadeInRequestAnimaionPutImageWave();
       });
-      // const timeOutId = window.setInterval(() => {
-      //   fadeInRequestAnimaionPutImageWave();
-      // }, 0.5);
-      // setArr.push(timeOutId);
     }
   }
 
   function prevPutImage(x, y, range) {
-    console.log('prev');
     if (x < 0 && y > height) {
       return;
     }
+
     objs.context.putImageData(
       imageData,
       0,
@@ -183,116 +127,115 @@ function fadeInFakerImage() {
       range,
       range
     );
-    // setTimeout(() => {
-    //   prevPutImage(x - range, y + range, range);
-    // }, 0.5);
     prevPutImage(x - range, y + range, range);
   }
-  function fadeInRequestAnimaionPutImage() {
-    objs.context.putImageData(imageData, 0, 0, x, y, 100, 100);
-    x += 100;
-    if (x >= objs.canvas.width) {
-      x = 0;
-      y += 100;
-    }
 
-    if (y < objs.canvas.height && x < objs.canvas.width) {
-      window.requestAnimationFrame(() => {
-        fadeInRequestAnimaionPutImage();
-      });
-    }
-  }
-  function fadeInRequestAnimaion() {
-    if (!visitedMap[x][y]) bfs(x, y, 40);
-    //console.log(x, y);
-    x++;
-    if (x >= objs.canvas.width) {
-      x = 0;
-      y += 80;
-    }
+  // 밑에는 안 쓰는 로직 아까워서 둠
+  // function fadeInRequestAnimaionPutImage() {
+  //   objs.context.putImageData(imageData, 0, 0, x, y, 100, 100);
+  //   x += 100;
+  //   if (x >= objs.canvas.width) {
+  //     x = 0;
+  //     y += 100;
+  //   }
 
-    if (y < objs.canvas.height && x < objs.canvas.width) {
-      window.requestAnimationFrame(() => {
-        fadeInRequestAnimaion();
-      });
-    }
-  }
+  //   if (y < objs.canvas.height && x < objs.canvas.width) {
+  //     window.requestAnimationFrame(() => {
+  //       fadeInRequestAnimaionPutImage();
+  //     });
+  //   }
+  // }
+  // function fadeInRequestAnimaion() {
+  //   if (!visitedMap[x][y]) bfs(x, y, 40);
+  //   //console.log(x, y);
+  //   x++;
+  //   if (x >= objs.canvas.width) {
+  //     x = 0;
+  //     y += 80;
+  //   }
 
-  function bfs(x, y, range) {
-    console.log('bfs', x, y);
-    // const q = [[x, y]];
-    const q = new Queue();
-    q.push([x, y]);
-    visitedMap[x][y] = true;
-    fillOneOfPixel(x, y);
-    while (q.length > 0) {
-      const [dx, dy] = q.shift();
+  //   if (y < objs.canvas.height && x < objs.canvas.width) {
+  //     window.requestAnimationFrame(() => {
+  //       fadeInRequestAnimaion();
+  //     });
+  //   }
+  // }
 
-      for (let [tx, ty] of [
-        [-1, 0],
-        [1, 0],
-        [0, -1],
-        [0, 1],
-      ]) {
-        const [nx, ny] = [dx + tx, dy + ty];
+  // function bfs(x, y, range) {
+  //   console.log('bfs', x, y);
+  //   // const q = [[x, y]];
+  //   const q = new Queue();
+  //   q.push([x, y]);
+  //   visitedMap[x][y] = true;
+  //   fillOneOfPixel(x, y);
+  //   while (q.length > 0) {
+  //     const [dx, dy] = q.shift();
 
-        // 현재 canvas 범위를 벗어나면
-        if (nx < 0 || nx >= height || ny < 0 || ny >= width) continue;
-        // 지정 범위안이 아니면
-        if (
-          nx < x - range ||
-          nx > x + range ||
-          ny < y - range ||
-          ny > y + range
-        )
-          continue;
-        if (visitedMap[nx][ny]) continue;
+  //     for (let [tx, ty] of [
+  //       [-1, 0],
+  //       [1, 0],
+  //       [0, -1],
+  //       [0, 1],
+  //     ]) {
+  //       const [nx, ny] = [dx + tx, dy + ty];
 
-        fillOneOfPixel(nx, ny);
-        visitedMap[nx][ny] = true;
-        q.push([nx, ny]);
-      }
-    }
-  }
+  //       // 현재 canvas 범위를 벗어나면
+  //       if (nx < 0 || nx >= height || ny < 0 || ny >= width) continue;
+  //       // 지정 범위안이 아니면
+  //       if (
+  //         nx < x - range ||
+  //         nx > x + range ||
+  //         ny < y - range ||
+  //         ny > y + range
+  //       )
+  //         continue;
+  //       if (visitedMap[nx][ny]) continue;
 
-  function fillPixel(dx, dy) {
-    console.log(123);
-    const pixelIndex = (dy * objs.canvas.width + dx) * 4;
-    const red = imageData.data[pixelIndex];
-    const green = imageData.data[pixelIndex + 1];
-    const blue = imageData.data[pixelIndex + 2];
-    const alpha = imageData.data[pixelIndex + 3];
+  //       fillOneOfPixel(nx, ny);
+  //       visitedMap[nx][ny] = true;
+  //       q.push([nx, ny]);
+  //     }
+  //   }
+  // }
 
-    // 픽셀 그리기
-    objs.context.fillStyle = `rgba(${red}, ${green}, ${blue}, ${alpha / 255})`;
+  // function fillPixel(dx, dy) {
+  //   console.log(123);
+  //   const pixelIndex = (dy * objs.canvas.width + dx) * 4;
+  //   const red = imageData.data[pixelIndex];
+  //   const green = imageData.data[pixelIndex + 1];
+  //   const blue = imageData.data[pixelIndex + 2];
+  //   const alpha = imageData.data[pixelIndex + 3];
 
-    objs.context.fillRect(dx, dy, 1, 1);
-    x++;
-    if (x >= objs.canvas.width) {
-      x = 0;
-      y++;
-    }
+  //   // 픽셀 그리기
+  //   objs.context.fillStyle = `rgba(${red}, ${green}, ${blue}, ${alpha / 255})`;
 
-    if (y < objs.canvas.height && x < objs.canvas.width) {
-      window.requestAnimationFrame(() => {
-        fillPixel(x, y);
-      });
-    }
-  }
+  //   objs.context.fillRect(dx, dy, 1, 1);
+  //   x++;
+  //   if (x >= objs.canvas.width) {
+  //     x = 0;
+  //     y++;
+  //   }
 
-  // 하나의 픽셀 그리기를 담당하는 함수
-  function fillOneOfPixel(x, y) {
-    const pixelIndex = (y * objs.canvas.width + x) * 4;
-    const red = imageData.data[pixelIndex];
-    const green = imageData.data[pixelIndex + 1];
-    const blue = imageData.data[pixelIndex + 2];
-    const alpha = imageData.data[pixelIndex + 3];
+  //   if (y < objs.canvas.height && x < objs.canvas.width) {
+  //     window.requestAnimationFrame(() => {
+  //       fillPixel(x, y);
+  //     });
+  //   }
+  // }
 
-    // 픽셀 그리기
-    objs.context.fillStyle = `rgba(${red}, ${green}, ${blue}, ${alpha / 255})`;
+  // // 하나의 픽셀 그리기를 담당하는 함수
+  // function fillOneOfPixel(x, y) {
+  //   const pixelIndex = (y * objs.canvas.width + x) * 4;
+  //   const red = imageData.data[pixelIndex];
+  //   const green = imageData.data[pixelIndex + 1];
+  //   const blue = imageData.data[pixelIndex + 2];
+  //   const alpha = imageData.data[pixelIndex + 3];
 
-    objs.context.fillRect(x, y, 1, 1);
-  }
+  //   // 픽셀 그리기
+  //   objs.context.fillStyle = `rgba(${red}, ${green}, ${blue}, ${alpha / 255})`;
+
+  //   objs.context.fillRect(x, y, 1, 1);
+  // }
 }
 
 function loadImages() {
@@ -301,7 +244,9 @@ function loadImages() {
   introFakerImage.src = './imgs/intro-fade-in-faker.png';
   introFakerImage.onload = () => {
     console.log('안녕하세요');
-    fadeInFakerImage();
+    setTimeout(() => {
+      fadeInFakerImage();
+    }, 2000);
   };
 
   sceneInfo[0].objs.introImages.push(introFakerImage);
