7bf0f645632289b7a0e2501086db28f2db83abec
SeungYn <kwls0407@gmail.com>
2024-06-10 17:07:33 +0900

    웹팩으로 스크립트 압축

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './main.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'main2.js',
+    path: path.resolve(__dirname, './'),
   },
 };
